5728dbe5581b3d385bc37124b3a76a274012d91b
anhhaibkhn <zorokingritsumei@gmail.com>
2021-01-09 21:19:29 +0900

    update CDF, IQR

--- exploratory_data_analysis_in_python/eda_python_chapter2.py
+++ exploratory_data_analysis_in_python/eda_python_chapter2.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from empiricaldist import Pmf
+from empiricaldist import Cdf
 
 class Distribution():
     ''' class to check the data distribution'''
@@ -28,19 +29,19 @@ class Distribution():
             print('[INFO]', msg)
             print(detail_msg)
 
-    def histogram(self, pd_series, str_label, x_str, y_str):
-        plt.hist(pd_series.dropna(), label = str_label)
+    def histogram(self, pd_series, str_label, x_str, y_str, bin_num):
+        plt.hist(pd_series.dropna(), label = str_label, bins = bin_num)
         plt.xlabel(x_str)
         plt.ylabel(y_str)
         plt.show()
 
-    def my_exec(self, msg):
+    def pmf_vs_histogram(self, msg):
         # first message
         self.verbose(msg,self.gss.head())
 
         # education histogram
         educ = self.gss['educ']
-        self.histogram(educ, 'educ', 'Years of education', 'Count')
+        self.histogram(educ, 'educ', 'Years of education', 'Count', 20)
 
         # PMF (Probability Mass Function)
         pmf_educ = Pmf.from_seq(educ, normalize = False)
@@ -57,8 +58,41 @@ class Distribution():
         plt.ylabel('PMF')
         plt.show()
 
+    def cdf_vs_pmf(self, msg):
+        # first message
+        self.verbose(msg,self.gss.head())
+        age = self.gss['age']
+
+        # check age PMF
+        # pmf_age = Pmf.from_seq(age, normalize = True)
+        # # plot age pmf
+        # pmf_age.bar(label = 'Age')
+        # plt.xlabel('Age')
+        # plt.ylabel('PMF')
+        # plt.show()
+
+        # check age CDF
+        cdf_age = Cdf.from_seq(age)
+        # cdf_age.plot()
+        # plt.xlabel('Age')
+        # plt.ylabel('CDF')
+        # plt.show()
+
+        # evaluate the CDF and the inverse CDF
+        q = 51 # age = 51
+        print(cdf_age(q)) # age 51 has around 0.66
+
+        p = 0.25 
+        print(cdf_age.inverse(p)) # probability = 0.25 inverse to age = 30. 
+
+        # check the IQR (interquartile range)
+
 
 
 # main 
 test = Distribution()
-test.my_exec('begin chapter 2')
+## PMF and Histogram 
+# test.pmf_vs_histogram('begin chapter 2')
+
+## CDF (Cumulative Distribution Functions)
+test.cdf_vs_pmf('Cumulative distribution functions')
