8210b40a57943813465f9b076e65e9f1b94258b0
Nguyen Hai <nguyenngochaibkhn@gmail.com>
2021-06-10 22:34:53 +0900

    update chpater 1_2

--- Working with Dates and Times in Python/chapter1_2.py
+++ Working with Dates and Times in Python/chapter1_2.py
@@ -102,6 +102,22 @@ class Combine_date_time(Date_Calendars):
         
         print(trip_counts)
 
+    def create_bike_dict(self, onebike_datetime_strings):
+        """ Parsing pairs of strings as datetimes """
+        # Write down the format string
+        fmt = "%Y-%m-%d %H:%M:%S"
+        # Initialize a list for holding the pairs of datetime objects
+        onebike_datetimes = []
+
+        # Loop over all trips
+        for (start, end) in onebike_datetime_strings:
+            trip = {'start': datetime.strptime(start, fmt),
+                    'end': datetime.strptime(end, fmt)}
+            
+            # Append the trip
+            onebike_datetimes.append(trip)
+
+
 
 
 
