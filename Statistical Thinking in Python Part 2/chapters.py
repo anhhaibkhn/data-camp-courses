4e66fa5cd5f659f2acb5d31ee1bc13fd5ea429aa
anhhaibkhn <nguyenngochaibkhn@gmail.com>
2021-07-16 14:13:32 +0900

    update chapter 1

--- Statistical Thinking in Python Part 2/chapters.py
+++ Statistical Thinking in Python Part 2/chapters.py
@@ -101,6 +101,32 @@ class Parameter:
         # Show the plot
         plt.show()
 
+    def pearson_r(self, x, y):
+        """Compute Pearson correlation coefficient between two arrays."""
+        # Compute correlation matrix: corr_mat
+        corr_mat = np.corrcoef(x, y)
+
+        # Return entry [0,1]
+        return corr_mat[0,1]
+
+    def correlation_plot(self, x, y ):
+        illiteracy, fertility = x, y
+        # Plot the illiteracy rate versus fertility
+        _ = plt.plot(illiteracy, fertility, marker='.', linestyle='none')
+
+        # Set the margins and label axes
+        plt.margins(0.02)
+        _ = plt.xlabel('percent illiterate')
+        _ = plt.ylabel('fertility')
+
+        # Show the plot
+        plt.show()
+
+        # Show the Pearson correlation coefficient
+        print(self.pearson_r(illiteracy, fertility))
+
+
+
         
 
 
