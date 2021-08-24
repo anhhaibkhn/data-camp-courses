8fb6f014561f566886e5d4ae1ff6e532d77ae9a4
Nguyen Hai <nguyenngochaibkhn@gmail.com>
2021-08-24 17:49:49 +0900

    update location bug fix

--- Statistical Thinking in Python Part 2/chapters.py
+++ Statistical Thinking in Python Part 2/chapters.py
@@ -27,14 +27,18 @@ import os
 
 
 import sys
-sys.path.insert(0, 'E:/data_science_resources/git_data_camp/data-camp-courses/Statistical Thinking in Python Part 1/')
+mydir =  sys.path[0]
+# import module from an upper directory 
+newdir = os.path.abspath(os.path.join(mydir, '..')) + '\Statistical Thinking in Python Part 1'
+print(newdir)
+sys.path.insert(0, newdir)
 from chapter_1_2_3_4 import Preparing_data as prep
 
 
 class Parameter:
     def __init__(self):
         
-        self.anscombe_data = pd.read_csv("E:/data_science_resources/git_data_camp/data-camp-courses/Statistical Thinking in Python Part 2/anscombe.csv")
+        self.anscombe_data = pd.read_csv("anscombe.csv")
         print(self.anscombe_data.info(verbose=True))
         # get x, y data
         self.x, self.y = self.anscombe_data.iloc[1:,0].to_numpy().astype(np.float64), self.anscombe_data.iloc[1:,1].to_numpy().astype(np.float64)
@@ -568,22 +572,64 @@ class Hypothesis(Bootstrap):
 
 
 
+    """ Chapter 4: Hypothesis test examples
+
+    As you saw from the last chapter, hypothesis testing can be a bit tricky. You need to define the null hypothesis, 
+    figure out how to simulate it, and define clearly what it means to be "more extreme" to compute the p-value. 
+    Like any skill, practice makes perfect, and this chapter gives you some good practice with hypothesis tests.
+    """
+    def diff_frac(self, data_A, data_B):        
+        frac_A = np.sum(data_A) / len(data_A)        
+        frac_B = np.sum(data_B) / len(data_B)
+        
+        return frac_B - frac_A
+
+    def calculate_p_value(self, data_A, data_B):
+        diff_frac_obs = self.diff_frac(data_A, data_B)
+
+        perm_replicates = self.draw_bs_reps(data_A, data_B, self.diff_frac)
+        
+        p_value = np.sum(perm_replicates >= diff_frac_obs) / 10000
+            
+        return p_value
+
+    def A_B_diff_test_1(self):  
+        # Construct arrays of data: dems, reps
+        dems = np.array([True] * 153 + [False] * 91)
+        reps = np.array([True] * 136 + [False] * 35)
+
+        def frac_yea_dems(dems, reps):
+            """Compute fraction of Democrat yea votes."""
+            frac = np.sum(dems) / len(dems)
+            return frac
+
+        # Acquire permutation samples: perm_replicates
+        perm_replicates = self.draw_perm_reps(dems, reps, frac_yea_dems, 10000)
+
+        # Compute and print p-value: p
+        p = np.sum(perm_replicates <= 153/244) / len(perm_replicates)
+        print('p-value =', p)
+  
+
+    def diff_of_means(self, data_1, data_2):
+            """Difference in means of two arrays."""
+            # The difference of means of data_1, data_2: diff
+            diff = np.mean(data_1) - np.mean(data_2)
+
+            return diff
+
+            
+    def A_B_diff_test_2(self, nht_dead, nht_live):
+        # Compute the observed difference in mean inter-no-hitter times: nht_diff_obs
+        nht_diff_obs = self.diff_of_means(nht_dead, nht_live)
+
+        # Acquire 10,000 permutation replicates of difference in mean no-hitter time: perm_replicates
+        perm_replicates = self.draw_perm_reps(nht_dead, nht_live, self.diff_of_means , 10000)
 
 
-
-
-
-
-
-""" Chapter 4: Hypothesis test examples
-
-As you saw from the last chapter, hypothesis testing can be a bit tricky. You need to define the null hypothesis, 
-figure out how to simulate it, and define clearly what it means to be "more extreme" to compute the p-value. 
-Like any skill, practice makes perfect, and this chapter gives you some good practice with hypothesis tests.
-"""
-
-
-
+        # Compute and print the p-value: p
+        p = np.sum(perm_replicates <= nht_diff_obs) / len(perm_replicates)
+        print('p-val =', p)
 
 
 
@@ -609,11 +655,43 @@ def main():
     chap3 = Hypothesis()
     # chap3.make_bee_swarm_plot(chap3.frog_df)
     # chap3.permutation_test_on_frog_data(chap3.force_a, chap3.force_b)
-    chap3.one_sample_bootstrap_hypothesis_test(chap3.force_b)
+    # chap3.one_sample_bootstrap_hypothesis_test(chap3.force_b)
+ 
+    nht_dead = np.array([  -1,  894,   10,  130,    1,  934,   29,    6,  485,  254,  372,
+                            81,  191,  355,  180,  286,   47,  269,  361,  173,  246,  492,
+                            462, 1319,   58,  297,   31, 2970,  640,  237,  434,  570,   77,
+                            271,  563, 3365,   89,    0,  379,  221,  479,  367,  628,  843,
+                            1613, 1101,  215,  684,  814,  278,  324,  161,  219,  545,  715,
+                            966,  624,   29,  450,  107,   20,   91, 1325,  124, 1468,  104,
+                            1309,  429,   62, 1878, 1104,  123,  251,   93,  188,  983,  166,
+                            96,  702,   23,  524,   26,  299,   59,   39,   12,    2,  308,
+                            1114,  813,  887])
+
+    nht_live = np.array([ 645, 2088,   42, 2090,   11,  886, 1665, 1084, 2900, 2432,  750,
+                        4021, 1070, 1765, 1322,   26,  548, 1525,   77, 2181, 2752,  127,
+                        2147,  211,   41, 1575,  151,  479,  697,  557, 2267,  542,  392,
+                        73,  603,  233,  255,  528,  397, 1529, 1023, 1194,  462,  583,
+                        37,  943,  996,  480, 1497,  717,  224,  219, 1531,  498,   44,
+                        288,  267,  600,   52,  269, 1086,  386,  176, 2199,  216,   54,
+                        675, 1243,  463,  650,  171,  327,  110,  774,  509,    8,  197,
+                        136,   12, 1124,   64,  380,  811,  232,  192,  731,  715,  226,
+                        605,  539, 1491,  323,  240,  179,  702,  156,   82, 1397,  354,
+                        778,  603, 1001,  385,  986,  203,  149,  576,  445,  180, 1403,
+                        252,  675, 1351, 2983, 1568,   45,  899, 3260, 1025,   31,  100,
+                        2055, 4043,   79,  238, 3931, 2351,  595,  110,  215,    0,  563,
+                        206,  660,  242,  577,  179,  157,  192,  192, 1848,  792, 1693,
+                        55,  388,  225, 1134, 1172, 1555,   31, 1582, 1044,  378, 1687,
+                        2915,  280,  765, 2819,  511, 1521,  745, 2491,  580, 2072, 6450,
+                        578,  745, 1075, 1103, 1549, 1520,  138, 1202,  296,  277,  351,
+                        391,  950,  459,   62, 1056, 1128,  139,  420,   87,   71,  814,
+                        603, 1349,  162, 1027,  783,  326,  101,  876,  381,  905,  156,
+                        419,  239,  119,  129,  467])
+
+    chap3.A_B_diff_test_2(nht_dead, nht_live)
+    
 
 
     
-
 
 
 if __name__ == "__main__":
