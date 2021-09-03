7e2931fbc8b1748216be55c4a8ce0c60611915e9
anhhaibkhn <nguyenngochaibkhn@gmail.com>
2021-09-03 17:51:52 +0900

    finisished new course

--- Statistical Thinking in Python Part 2/chapters.py
+++ Statistical Thinking in Python Part 2/chapters.py
@@ -735,6 +735,12 @@ class Darwin_finches(Hypothesis):
 
         # print(df_bdepth.info(), '\n', df_bdepth.head())
 
+        scandens_data = pd.read_csv('scandens_beak_depth_heredity.csv',  header=0)
+        fortis_data   = pd.read_csv('fortis_beak_depth_heredity.csv', header=0)
+
+
+
+
     def create_bee_swarm_plot(self, df):
         # Create bee swarm plot
         _ = sns.swarmplot(x = 'year', y = 'beak_depth',data= df)
@@ -922,9 +928,109 @@ class Darwin_finches(Hypothesis):
         print('2012: mean ratio =', mean_ratio_2012,
             'conf int =', conf_int_2012)
 
+
+    def EDA_heritability(self,bd_parent_scandens, bd_offspring_scandens, bd_parent_fortis, bd_offspring_fortis):
+        """ Make a scatter plot of the average offspring beak depth (y-axis) versus average parental beak depth (x-axis) for both species. 
+        Use the alpha=0.5 keyword argument to help you see overlapping points. """
+        
+        # Make scatter plots
+        _ = plt.plot(bd_parent_fortis, bd_offspring_fortis,
+                    marker='.', linestyle='none', color='blue', alpha=0.5)
+        _ = plt.plot(bd_parent_scandens, bd_offspring_scandens,
+                    marker='.', linestyle='none', color='red' , alpha=0.5)
+
+        # Label axes
+        _ = plt.xlabel('parental beak depth (mm)')
+        _ = plt.ylabel('offspring beak depth (mm)')
+
+        # Add legend
+        _ = plt.legend(('G. fortis', 'G. scandens'), loc='lower right')
+
+        # Show plot
+        plt.show()
+
+    def draw_bs_pairs(x, y, func, size=1):
+        """Perform pairs bootstrap for a single statistic."""
+
+        # Set up array of indices to sample from: inds
+        inds = np.arange(len(x))
+
+        # Initialize replicates: bs_replicates
+        bs_replicates = np.empty(size)
+
+        # Generate replicates
+        for i in range(size):
+            bs_inds = np.random.choice(inds, size=len(inds))
+            bs_x, bs_y = x[bs_inds], y[bs_inds]
+            bs_replicates[i] = func(bs_x, bs_y)
+
+        return bs_replicates
+
     
+    def Pearson_correlation_coefficient(self,bd_parent_scandens, bd_offspring_scandens, bd_parent_fortis, bd_offspring_fortis, pearson_r, draw_bs_pairs):
+        """ Pearson correlation of offspring and parental data """
+        # Compute the Pearson correlation coefficients
+        r_scandens = pearson_r(bd_parent_scandens, bd_offspring_scandens)
+        r_fortis = pearson_r(bd_parent_fortis,  bd_offspring_fortis)
+
+        # Acquire 1000 bootstrap replicates of Pearson r
+        bs_replicates_scandens = draw_bs_pairs(bd_parent_scandens, bd_offspring_scandens,pearson_r, 1000)
+
+        bs_replicates_fortis = draw_bs_pairs(bd_parent_fortis,bd_offspring_fortis , pearson_r, 1000)
 
 
+        # Compute 95% confidence intervals
+        conf_int_scandens = np.percentile(bs_replicates_scandens, [2.5, 97.5])
+        conf_int_fortis = np.percentile(bs_replicates_fortis, [2.5, 97.5])
+
+        # Print results
+        print('G. scandens:', r_scandens, conf_int_scandens)
+        print('G. fortis:', r_fortis, conf_int_fortis)
+
+    def Exercise_Measuring_heritability(self,bd_parent_scandens, bd_offspring_scandens, bd_parent_fortis, bd_offspring_fortis,draw_bs_pairs):
+            def heritability(parents, offspring):
+                """Compute the heritability from parent and offspring samples."""
+                covariance_matrix = np.cov(parents, offspring)
+                return covariance_matrix[0,1] / covariance_matrix[0,0]
+
+            # Compute the heritability
+            heritability_scandens = heritability(bd_parent_scandens, bd_offspring_scandens)
+            heritability_fortis = heritability(bd_parent_fortis, bd_offspring_fortis)
+
+            # Acquire 1000 bootstrap replicates of heritability
+            replicates_scandens = draw_bs_pairs(
+                    bd_parent_scandens, bd_offspring_scandens, heritability, size= 1000)
+                    
+            replicates_fortis = draw_bs_pairs(
+                    bd_parent_fortis, bd_offspring_fortis, heritability, size= 1000)
+
+
+            # Compute 95% confidence intervals
+            conf_int_scandens = np.percentile(replicates_scandens, [2.5, 97.5])
+            conf_int_fortis = np.percentile(replicates_fortis, [2.5, 97.5])
+
+            # Print results
+            print('G. scandens:', heritability_scandens, conf_int_scandens)
+            print('G. fortis:', heritability_fortis, conf_int_fortis)
+
+    
+    def beak_depth_heritable (self,bd_parent_scandens, bd_offspring_scandens,heritability_scandens, heritability ):
+        """ Is beak depth heritable at all in G. scandens?"""
+        # Initialize array of replicates: perm_replicates
+        perm_replicates = np.empty(10000)
+
+        # Draw replicates
+        for i in range(10000):
+            # Permute parent beak depths
+            bd_parent_permuted = np.random.permutation(bd_parent_scandens)
+            perm_replicates[i] = heritability(bd_parent_permuted, bd_offspring_scandens )
+
+
+        # Compute p-value: p
+        p = np.sum(perm_replicates >= heritability_scandens) / len(perm_replicates)
+
+        # Print the p-value
+        print('p-val =', p)
 
 
 
